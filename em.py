14ca7f1fa04d6a66af61bb2b6e676b229bb6cde8
sergioo-herreraa <112037477+sergioo-herreraa@users.noreply.github.com>
2025-08-22 13:54:43 -0600

    Update em.py

--- em.py
+++ em.py
@@ -5,6 +5,28 @@ from scipy.special import logsumexp
 from common import GaussianMixture
 from scipy.stats import multivariate_normal
 
+def observed_values_Cu(X, u, j, mixture):
+    x = X[u, :]                        
+    mask = x != 0
+    Cu = np.where(mask, 1, 0)
+
+    x_cu = x[mask]
+    size = len(x_cu)                   
+    miu_cu = mixture.mu[j, mask]     
+    var_cu = mixture.var[j]*np.identity(size) 
+
+    return Cu, x_cu, miu_cu, var_cu
+
+def f_u_j(X, u, j, mixture):
+  Cu, x_cu, miu_cu, var_cu = observed_values_Cu(X, u, j, mixture)
+  gaussian = multivariate_normal.pdf(x_cu, miu_cu, var_cu)
+  return np.log(mixture.p[j])+np.log(gaussian)
+
+def log_pj_given_u(X, u, j, mixture):
+  K = len(mixture.var)
+  sum_exp = np.sum([np.exp(f_u_j(X, u, i, mixture)) for i in range(K)])
+  return f_u_j(X, u, j, mixture)-np.log(sum_exp)
+
 def estep(X: np.ndarray, mixture: GaussianMixture) -> Tuple[np.ndarray, float]:
     """E-step: Softly assigns each datapoint to a gaussian component
 
@@ -18,39 +40,21 @@ def estep(X: np.ndarray, mixture: GaussianMixture) -> Tuple[np.ndarray, float]:
         float: log-likelihood of the assignment
 
     """
-    n, _ = X.shape
-    K, _ = mixture.mu.shape
-    post = np.zeros((n, K))
+    n, d = X.shape
+    K = len(mixture.var)
+    p = np.zeros((n, K))
+    log_likelihood_n=0
 
-    ll = 0
-    for i in range(n):
-        mask = (X[i, :] != 0)
+    for u in range(n):
+        log_likelihood_k= 0
         for j in range(K):
-            log_likelihood = log_gaussian(X[i, mask], mixture.mu[j, mask],
-                                          mixture.var[j])
-            post[i, j] = np.log(mixture.p[j] + 1e-16) + log_likelihood
-        total = logsumexp(post[i, :])
-        post[i, :] = post[i, :] - total
-        ll += total
+            p[u, j] = log_pj_given_u(X, u, j, mixture)
+            log_likelihood_k += f_u_j(X, u, j, mixture)
 
-    return np.exp(post), ll
+        log_likelihood_n += log_likelihood_k
 
-
-def log_gaussian(x: np.ndarray, mean: np.ndarray, var: float) -> float:
-    """Computes the log probablity of vector x under a normal distribution
-
-    Args:
-        x: (d, ) array holding the vector's coordinates
-        mean: (d, ) mean of the gaussian
-        var: variance of the gaussian
-
-    Returns:
-        float: the log probability
-    """
-    d = len(x)
-    log_prob = -d / 2.0 * np.log(2 * np.pi * var)
-    log_prob -= 0.5 * ((x - mean)**2).sum() / var
-    return log_prob
+    return np.exp(p), log_likelihood_n
+    
 
 def mstep(X: np.ndarray, post: np.ndarray, mixture: GaussianMixture,
           min_variance: float = .25) -> GaussianMixture:
@@ -66,35 +70,39 @@ def mstep(X: np.ndarray, post: np.ndarray, mixture: GaussianMixture,
 
     Returns:
         GaussianMixture: the new gaussian mixture
-    """
+    """ 
     n, d = X.shape
-    _, K = post.shape
+    K = len(mixture.var)
+    miu= mixture.mu
+    var = mixture.var
+    p = mixture.p
 
-    n_hat = post.sum(axis=0)
-    p = n_hat / n
-
-    mu = mixture.mu.copy()
-    var = np.zeros(K)
-
-    for j in range(K):
-        sse, weight = 0, 0
+    for k in range(K):
         for l in range(d):
-            mask = (X[:, l] != 0)
-            n_sum = post[mask, j].sum()
-            if (n_sum >= 1):
-                # Updating mean
-                mu[j, l] = (X[mask, l] @ post[mask, j]) / n_sum
-            # Computing variance
-            sse += ((mu[j, l] - X[mask, l])**2) @ post[mask, j]
-            weight += n_sum
-        var[j] = sse / weight
-        if var[j] < min_variance:
-            var[j] = min_variance
+            num_mu = 0
+            denom_mu= 0
+            num_var = 0
+            denom_var = 0
+            p_index = 0
+            for u in range(n):
+              Cu, x_cu, miu_cu, var_cu = observed_values_Cu(X, u, k, mixture)
+              num_mu += post[u, k]*Cu[l]*X[u,l]
+              denom_mu += post[u, k]*Cu[l]
+              num_var += post[u, k]*np.abs(np.linalg.norm(x_cu-miu_cu))**2
+              denom_var += post[u, k]*len(x_cu)
+              p_index+=post[u, k]
 
-    return GaussianMixture(mu, var, p)
+            if denom_mu >=1:
+                miu[k,l] = num_mu/denom_mu
+          
+        var[k] = max(num_var/denom_var, 0.25)
 
-def run(X: np.ndarray, mixture: GaussianMixture,
-        post: np.ndarray) -> Tuple[GaussianMixture, np.ndarray, float]:
+        p[k] = p_index/n
+
+    return GaussianMixture(miu, var, p)
+    
+          
+def run(X: np.ndarray, mixture: GaussianMixture) -> Tuple[GaussianMixture, np.ndarray, float]:
     """Runs the mixture model
 
     Args:
@@ -108,12 +116,35 @@ def run(X: np.ndarray, mixture: GaussianMixture,
             for all components for all examples
         float: log-likelihood of the current assignment
     """
+    old_log_likelihood= None
+    log_likelihood= None
 
-    prev_ll = None
-    ll = None
-    while (prev_ll is None or ll - prev_ll > 1e-6 * np.abs(ll)):
-        prev_ll = ll
-        post, ll = estep(X, mixture)
-        mixture = mstep(X, post, mixture)
+    while (old_log_likelihood is None or log_likelihood - old_log_likelihood > 1e-6*np.abs(log_likelihood)):
+      old_log_likelihood = log_likelihood
+      post, log_likelihood = estep(X, mixture)
+      mixture = mstep(X, post, mixture)
 
-    return mixture, post, ll
+    return mixture, post, log_likelihood
+
+
+
+def fill_matrix(X: np.ndarray, mixture: GaussianMixture, K, seed) -> np.ndarray:
+    """Fills an incomplete matrix according to a mixture model
+
+    Args:
+        X: (n, d) array of incomplete data (incomplete entries =0)
+        mixture: a mixture of gaussians
+
+    Returns
+        np.ndarray: a (n, d) array with completed data
+    """
+    mixture, post = common.init(X, K, seed)
+    mixture, post, log_likelihood = run(X, mixture)
+    n, d = X.shape
+    
+    for u in range(n):
+        for l in range(d):
+            if X[u, l]==0:
+                k=np.argmax(post[u, :])
+                X[u, l]=mixture.mu[k, l]
+    return X
